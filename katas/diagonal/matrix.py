50863559d0298241cea7b75ae2b6a72dfb829460
Henrique Albor <henrique.inftec@hotmail.com>
2017-02-08 01:08:34 -0200

    creates test cases for hackerrank example.

--- katas/diagonal/matrix.py
+++ katas/diagonal/matrix.py
@@ -4,18 +4,18 @@ class Matrix:
         self.elements = elements or ([1, 2, 3], [4, 5, 6], [7, 8, 9])
 
 
-    def left_diagonal_sum(self):
+    def primary_diagonal_sum(self):
         return sum(element for line_no, line in enumerate(self)
                    for element_no, element in enumerate(line)
                    if line_no == element_no)
 
-    def right_diagonal_sum(self):
+    def secondary_diagonal_sum(self):
         return sum(element for line_no, line in enumerate(self)
                    for element_no, element in enumerate(line)
                    if line_no + element_no == 2)
 
     def diagonal_difference(self):
-        return abs(self.left_diagonal_sum() - self.right_diagonal_sum())
+        return abs(self.primary_diagonal_sum() - self.secondary_diagonal_sum())
 
     def __iter__(self):
         return iter(self.elements)
--- katas/diagonal/test_diagonal.py
+++ katas/diagonal/test_diagonal.py
@@ -25,47 +25,61 @@ class MatrixTest(TestCase):
         expected = [[1,2,3],[4,5,6],[7,8,9]]
         self.assertSequenceEqual(expected, self.matrix.elements)
 
-    def test_left_diagonal_sum_should_be_15(self):
-        self.assertEqual(15, self.matrix.left_diagonal_sum())
+    def test_primary_diagonal_sum_should_be_15(self):
+        self.assertEqual(15, self.matrix.primary_diagonal_sum())
 
-    def test_right_diagonal_sum_should_be_15(self):
-        self.assertEqual(15, self.matrix.right_diagonal_sum())
+    def test_secondary_diagonal_sum_should_be_15(self):
+        self.assertEqual(15, self.matrix.secondary_diagonal_sum())
 
-    def test_left_diagonal_sum_should_be_9_when_value_is_modified(self):
+    def test_primary_diagonal_sum_should_be_9_when_value_is_modified(self):
         # 2 2 3
         # 4 3 5
         # 7 8 5
-        self.change_left_diagonal_values()
-        self.assertEqual(10, self.matrix.left_diagonal_sum())
+        self.change_primary_diagonal_values()
+        self.assertEqual(10, self.matrix.primary_diagonal_sum())
 
-    def test_right_diagonal_sum_should_be_9_when_value_is_modified(self):
-        self.change_right_diagonal_values()
-        self.assertEqual(9, self.matrix.right_diagonal_sum())
+    def test_secondary_diagonal_sum_should_be_9_when_values_are_modified(self): #noqa
+        self.change_secondary_diagonal_values()
+        self.assertEqual(9, self.matrix.secondary_diagonal_sum())
 
-    def test_diagonal_difference_should_be_zero(self):
+    def test_difference_between_diagonals_should_be_zero(self):
         self.assertEqual(0, self.matrix.diagonal_difference())
 
-    def test_diagonal_difference_should_be_3_when_left_diagonal_values_are_modified(self): #noqa
+    def testt_difference_between_diagonals_should_be_3_when_primary_values_are_modified(self): #noqa
         # 2 2 3
         # 4 3 5
         # 7 8 5
-        self.change_left_diagonal_values()
+        self.change_primary_diagonal_values()
         self.assertEqual(3, self.matrix.diagonal_difference())
 
-    def test_diagonal_difference_should_be_4_when_right_diagonal_values_are_modified(self): #noqa
+    def test_difference_between_diagonals_should_be_4_when_secondary_values_are_modified(self): #noqa
         # 1 2 1
         # 4 3 6
         # 5 8 9
-        self.change_right_diagonal_values()
+        self.change_secondary_diagonal_values()
         self.assertEqual(4, self.matrix.diagonal_difference())
 
-    def change_left_diagonal_values(self):
+    def change_primary_diagonal_values(self):
         self.matrix.elements[0][0] = 2
         self.matrix.elements[1][1] = 3
         self.matrix.elements[2][2] = 5
 
-    def change_right_diagonal_values(self):
+    def change_secondary_diagonal_values(self):
         self.matrix.elements[0][2] = 1
         self.matrix.elements[1][1] = 3
         self.matrix.elements[2][0] = 5
+
+class HackerRankMatrixTest(TestCase):
+
+    def setUp(self):
+        self.matrix = Matrix(elements=[(11, 2, 4), (4, 5, 6), (10, 8, -12)])
+
+    def test_primary_diagonal_sum_should_be_4(self):
+        self.assertEquals(4, self.matrix.primary_diagonal_sum())
+
+    def test_secondary_diagonal_sum_should_be_15(self):
+        self.assertEquals(19, self.matrix.secondary_diagonal_sum())
+
+    def test_difference_between_diagonals_should_be_4(self): #noqa
+        self.assertEqual(15, self.matrix.diagonal_difference())
 

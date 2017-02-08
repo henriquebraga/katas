41c03831c5ecfa96d142676629a57d681ceb8277
Henrique Albor <henrique.inftec@hotmail.com>
2017-02-08 21:21:27 -0200

    Fixes generic cases for matrix (4x4, 5x5 etc)

--- katas/diagonal/matrix.py
+++ katas/diagonal/matrix.py
@@ -3,7 +3,6 @@ class Matrix:
     def __init__(self, elements=None):
         self.elements = elements or ([1, 2, 3], [4, 5, 6], [7, 8, 9])
 
-
     def primary_diagonal_sum(self):
         return sum(element for line_no, line in enumerate(self)
                    for element_no, element in enumerate(line)
@@ -12,13 +11,14 @@ class Matrix:
     def secondary_diagonal_sum(self):
         return sum(element for line_no, line in enumerate(self)
                    for element_no, element in enumerate(line)
-                   if line_no + element_no == 2)
+                   if line_no + element_no == len(line) - 1)
 
     def diagonal_difference(self):
         return abs(self.primary_diagonal_sum() - self.secondary_diagonal_sum())
 
     def __iter__(self):
         return iter(self.elements)
+
 
 
 
--- katas/diagonal/test_diagonal.py
+++ katas/diagonal/test_diagonal.py
@@ -3,7 +3,8 @@ from unittest import TestCase
 from katas.diagonal.matrix import Matrix
 
 
-class MatrixTest(TestCase):
+class DefaultMatrixTest(TestCase):
+    '''Tests for matrix basic operations (no parameters are passed when initialized)'''
 
     def setUp(self):
         self.matrix = Matrix()
@@ -14,7 +15,7 @@ class MatrixTest(TestCase):
     def test_matrix_should_be_iterable(self):
         self.assertTrue(getattr(self.matrix, '__iter__'))
 
-    def test_all_lines_should_have_three_elements(self):
+    def test_all_matrix_lines_should_have_three_elements(self):
         self.assertTrue(all(len(n) == 3 for n in self.matrix))
 
     def test_matrix_should_have_nine_elements(self):
@@ -69,7 +70,9 @@ class MatrixTest(TestCase):
         self.matrix.elements[1][1] = 3
         self.matrix.elements[2][0] = 5
 
-class HackerRankMatrixTest(TestCase):
+
+class MatrixInitializedWithNoDefaultValuesTest(TestCase):
+    '''Test cases for a matrix when is not initialized passing elements.'''
 
     def setUp(self):
         self.matrix = Matrix(elements=[(11, 2, 4), (4, 5, 6), (10, 8, -12)])
@@ -82,4 +85,35 @@ class HackerRankMatrixTest(TestCase):
 
     def test_difference_between_diagonals_should_be_4(self): #noqa
         self.assertEqual(15, self.matrix.diagonal_difference())
+
+
+class Matrix4x4Test(TestCase):
+    '''Test cases for a 4x4 Matrix.'''
+
+    def setUp(self):
+        self.matrix = Matrix(
+            elements=[
+                (1,2,3,4),
+                (5,6,7,8),
+                (9,10,11,12),
+                (13,14,15,16)
+            ]
+        )
+
+    def test_all_matrix_lines_should_have_four_elements(self):
+        self.assertTrue(all(len(n) == 4 for n in self.matrix))
+
+    def test_matrix_should_have_sixteen_elements(self):
+        total = sum(len(line) for line in self.matrix)
+        self.assertEquals(16, total)
+
+    def test_primary_diagonal_sum_should_be_34(self):
+        self.assertEqual(34, self.matrix.primary_diagonal_sum())
+
+    def test_secondary_diagonal_sum_should_be_34(self):
+        self.assertEqual(34, self.matrix.secondary_diagonal_sum())
+
+    def test_difference_between_diagonals_should_be_0(self):
+        self.assertEqual(0, self.matrix.diagonal_difference())
+
 
